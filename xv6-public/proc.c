f2c0eedee1265612ff37c7c3373dfcc614728e3b
kains123 <kains0417@naver.com>
2023-04-15 22:00:10 +0900

    Update proc.c

--- xv6-public/proc.c
+++ xv6-public/proc.c
@@ -81,11 +81,11 @@ int mlfq_enqueue(int lev, struct proc *p)
   if (queue->size == NPROC)
     return -1;
 
-  // queue->rear = (queue->rear + 1) % NPROC;
-  // queue->data[queue->rear] = p;
-  // ++queue->size;
+  queue->rear = (queue->rear + 1) % NPROC;
+  queue->data[queue->rear] = p;
+  ++queue->size;
 
-  // p->level = lev;
+  p->level = lev;
 
   return 0;
 }
