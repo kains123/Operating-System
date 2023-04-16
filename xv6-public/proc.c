01a96abffc58d1a52e9b9430d0a8a2d00d551f3f
kains123 <kains0417@naver.com>
2023-04-16 21:16:43 +0900

    Update proc.c

--- xv6-public/proc.c
+++ xv6-public/proc.c
@@ -579,22 +579,22 @@ scheduler(void)
     cprintf("&&&&&&&&&&&&"); //! TODO set priority 
     if(p != 0)
     {
-    //   if(p->state != RUNNABLE)
-    //     continue;
-    //   // Switch to chosen process.  It is the process's job
-    //   // to release ptable.lock and then reacquire it
-    //   // before jumping back to us.
-    //   c->proc = p;
-    //   switchuvm(p);
-    //   p->state = RUNNING;
+      if(p->state != RUNNABLE)
+        continue;
+      // Switch to chosen process.  It is the process's job
+      // to release ptable.lock and then reacquire it
+      // before jumping back to us.
+      c->proc = p;
+      switchuvm(p);
+      p->state = RUNNING;
 
-    //   swtch(&(c->scheduler), p->context);
-    //   switchkvm();
+      swtch(&(c->scheduler), p->context);
+      switchkvm();
 
-    //   // Process is done running for now.
-    //   // It should have changed its p->state before coming back.
-    //   c->proc = 0;
-    // }
+      // Process is done running for now.
+      // It should have changed its p->state before coming back.
+      c->proc = 0;
+    }
 
     if(mlfq_manager.global_executed_ticks >= MLFQ_GLOBAL_BOOSTING_TICK_INTERVAL) {
       // mlfq_priority_boost();
