5e6c2e399f2f20868dc95bdbfee445f0092231f6
kains123 <kains0417@naver.com>
2023-04-18 00:01:14 +0900

    Update proc.c

--- xv6-public/main.c
+++ xv6-public/main.c
@@ -27,7 +27,6 @@ main(void)
   consoleinit();   // console hardware
   uartinit();      // serial port
   pinit();         // process table
-  mlfq_init();
   tvinit();        // trap vectors
   binit();         // buffer cache
   fileinit();      // file table
