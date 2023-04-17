47fb0a7ccb2d0376e484ad9eb48c08f6028981ad
kains123 <kains0417@naver.com>
2023-04-18 01:50:40 +0900

    Update proc.c

--- xv6-public/mlfq_test.c
+++ xv6-public/mlfq_test.c
@@ -41,7 +41,7 @@ int fork_children2()
       // {
       //   printf(1, "setpriority returned %d\n", r);
       // }
-      exit();
+        exit();
     }
   }
   return parent;
@@ -76,38 +76,36 @@ int main(int argc, char *argv[])
 	int ret_val; 
 	ret_val = myfunction(buf);
 	printf(1, "Return value : 0x%x\n", ret_val);
-  // int i, pid;
-  // int count[MAX_LEVEL] = {0};
-  // // int child;
+	exit();
+//   int i, pid;
+//   int count[MAX_LEVEL] = {0};
+// //  int child;
 
-  // parent = getpid();
+//   parent = getpid();
 
-  // //schedulerLock test 
-  // //schedulerLock test
+//   printf(1, "MLFQ test start\n");
 
-  printf(1, "MLFQ test start\n");
+//   printf(1, "[Test 1] default\n");
+//   pid = fork_children();
 
-  // printf(1, "[Test 1] default\n");
-  // pid = fork_children();
-
-  // if (pid != parent)
-  // {
-  //   for (i = 0; i < NUM_LOOP; i++)
-  //   {
-  //     int x = getLevel();
-  //     if (x < 0 || x > 4)
-  //     {
-  //       printf(1, "Wrong level: %d\n", x);
-  //       exit();
-  //     }
-  //     count[x]++;
-  //   }
-  //   printf(1, "Process %d\n", pid);
-  //   for (i = 0; i < MAX_LEVEL; i++)
-  //     printf(1, "L%d: %d\n", i, count[i]);
-  // }
-  // exit_children();
-  // printf(1, "[Test 1] finished\n");
-  // printf(1, "done\n");
-  exit();
+//   if (pid != parent)
+//   {
+//     for (i = 0; i < NUM_LOOP; i++)
+//     {
+//       int x = getLevel();
+//       if (x < 0 || x > 4)
+//       {
+//         printf(1, "Wrong level: %d\n", x);
+//         exit();
+//       }
+//       count[x]++;
+//     }
+//     printf(1, "Process %d\n", pid);
+//     for (i = 0; i < MAX_LEVEL; i++)
+//       printf(1, "L%d: %d\n", i, count[i]);
+//   }
+//   exit_children();
+//   printf(1, "[Test 1] finished\n");
+//   printf(1, "done\n");
+//   exit();
 }
