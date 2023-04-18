4b40d9ce20ab33ef5d1cfe956146453a27fcd114
kains123 <kains0417@naver.com>
2023-04-19 03:07:09 +0900

    Update proc.c

--- xv6-public/proc.c
+++ xv6-public/proc.c
@@ -695,14 +695,11 @@ scheduler(void)
         }
         if(lockedproc->state == RUNNABLE) {
           lockedproc->state = RUNNING;
-        }
-      } else {
-        (mlfq_manager.global_executed_ticks)++;
-        if(mlfq_manager.global_executed_ticks >= MLFQ_GLOBAL_BOOSTING_TICK_INTERVAL){
-          //if there is a lock just remove it!
-          withdraw_lock();
+        } else {
+           cprintf("WHAT TO DO?");
         }
       }
+      //Context Switching
       p = lockedproc;
       c->proc = p;
       switchuvm(p);
@@ -712,6 +709,11 @@ scheduler(void)
       // Process is done running for now.
       // It should have changed its p->state before coming back.
       c->proc = 0;
+      (mlfq_manager.global_executed_ticks)++;
+      if(mlfq_manager.global_executed_ticks >= MLFQ_GLOBAL_BOOSTING_TICK_INTERVAL){
+          //if there is a lock just remove it!
+          withdraw_lock();
+      }
     } else {
       p = mlfq_select(); //select mlfq which to execute.
 
