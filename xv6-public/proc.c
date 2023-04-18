1a574ba561905ed729add842af926decca77f09f
kains123 <kains0417@naver.com>
2023-04-19 00:02:22 +0900

    Update proc.c

--- xv6-public/proc.c
+++ xv6-public/proc.c
@@ -248,19 +248,19 @@ mlfq_select()
       for (i = 0; i < size; ++i)
       {
         ret = queue->data[queue->front];
-        // if(ret->state != RUNNABLE) {
-        //   cprintf("!!!!!!!!!![3]!!!!!!!!!!\n");
-        //   mlfq_dequeue(lev, 0); //remove first process in queue (lev).
-        //   mlfq_enqueue(lev, ret); //add again in the end of queue (lev).
-        // }
-        // else {
+        if(ret->state != RUNNABLE) {
+          
+          cprintf("!!!!!!!!!![3: RET_PID: %d]!!!!!!!!!!\n", ret->pid);
+          mlfq_dequeue(lev, 0); //remove first process in queue (lev).
+          mlfq_enqueue(lev, ret); //add again in the end of queue (lev).
+        }
+        else {
           if(ret->state == RUNNABLE) {
-            cprintf("!!!!!!!!!![3]!!!!!!!!!!\n");
             cprintf("[%d] name: %s\n", ret->pid, ret->name);
             goto found;
           }
         }
-      // }
+      }
     } else if (lev == 2) {
         cprintf("********level %d********\n", lev);
         //find the lowest priority in L2 queue
