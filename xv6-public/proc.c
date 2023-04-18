fbcd0a44dcc9d427a3d5463e337aad3d6aff5401
kains123 <kains0417@naver.com>
2023-04-19 00:40:53 +0900

    Update proc.c

--- xv6-public/proc.c
+++ xv6-public/proc.c
@@ -679,7 +679,7 @@ scheduler(void)
   c->proc = 0;
   
   for(;;){
-    print_mlfq();
+    // print_mlfq();
     // Enable interrupts on this processor.
     sti();
     // Loop over process table looking for process to run.
