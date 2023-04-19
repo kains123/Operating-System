861f88abf2c306326ac921934b1499a0b7330439
kains123 <kains0417@naver.com>
2023-04-19 19:27:59 +0900

    Update proc.c

--- xv6-public/proc.c
+++ xv6-public/proc.c
@@ -249,7 +249,7 @@ mlfq_select()
         if(ret->state == RUNNABLE) {
           goto found;
         } else {
-          cprinf("****************");
+          cprintf("****************");
         }
       }
     } else if (lev == 2) {
