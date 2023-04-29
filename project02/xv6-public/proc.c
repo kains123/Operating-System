c251ce0b48fc24c5e6c370254000e9e58a07c5d6
kains123 <kains0417@naver.com>
2023-04-29 18:42:32 +0900

    Update Project 02

--- project02/xv6-public/proc.c
+++ project02/xv6-public/proc.c
@@ -537,7 +537,7 @@ procdump(void)
 
 int thread_create(thread_t *thread, void *(*start_routine)(void*), void *arg)
 {
-
+  return 0;
 }
 
 void thread_exit(void *retval)
@@ -546,5 +546,5 @@ void thread_exit(void *retval)
 }
 
 int thread_join(thread_t thread, void **retval){
-
+  return 0;
 }
