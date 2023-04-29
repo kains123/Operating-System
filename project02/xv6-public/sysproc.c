f1bb45d5e9702b07cbb9d6b3b84430f8c9bab8fc
kains123 <kains0417@naver.com>
2023-04-29 18:50:52 +0900

    Update Project 02

--- project02/xv6-public/sysproc.c
+++ project02/xv6-public/sysproc.c
@@ -89,3 +89,21 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+int
+sys_thread_create(void)
+{
+  return 0;
+}
+
+int
+sys_thread_exit(void)
+{
+  return 0;
+}
+
+int
+sys_thread_join(void)
+{
+  return 0;
+}
