0dd9ee215f804eadfa70b111fec3460697386e41
kains123 <kains0417@naver.com>
2023-05-14 15:07:15 +0900

    Update 2019087192.c

--- project02/xv6-public/proc.c
+++ project02/xv6-public/proc.c
@@ -387,6 +387,7 @@ scheduler(void)
   struct thread *t;
   struct cpu *c = mycpu();
   c->proc = 0;
+  
 
   for(;;){
     // Enable interrupts on this processor.
@@ -395,18 +396,65 @@ scheduler(void)
 
     acquire(&ptable.lock);
    
-    p = proc_choose();
-    t = p ? &CURTHREAD(p) : 0;
-    if(p != 0){
-      cprintf("########scheduler3########\n");
-      c->proc = p;
-      switchuvm(p);
-      t->state = RUNNING;
-      swtch(&(c->scheduler), p->context);
-      switchkvm();
-      // Process is done running for now.
-      // It should have changed its p->state before coming back.
-      c->proc = 0;
+    // p = proc_choose();
+    // t = p ? &CURTHREAD(p) : 0;
+    // if(p != 0){
+    //   cprintf("########scheduler3########\n");
+    //   c->proc = p;
+    //   switchuvm(p);
+    //   t->state = RUNNING;
+    //   swtch(&(c->scheduler), p->context);
+    //   switchkvm();
+    //   // Process is done running for now.
+    //   // It should have changed its p->state before coming back.
+    //   c->proc = 0;
+    // }
+    acquire(&ptable.lock);
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+      int start = 0;
+      if(p->state != RUNNABLE)
+        continue;
+      // t = p ? &CURTHREAD(p) : 0;
+      if(p != 0) {
+        for (t = &CURTHREAD(p); ; ++t)
+        {
+          if (t == &p->threads[NTHREAD])
+            t = &p->threads[0];
+
+          if (t->state == RUNNABLE)
+            break;
+
+          if (start && t == &CURTHREAD(p))
+            panic("invalid logic");
+          start = 1;
+        }
+        p->curtid = t - p->threads;
+        // return p;
+        t = p ? &CURTHREAD(p) : 0;
+        cprintf("########scheduler3########\n");
+        c->proc = p;
+        switchuvm(p);
+        t->state = RUNNING;
+        swtch(&(c->scheduler), p->context);
+        switchkvm();
+        // Process is done running for now.
+        // It should have changed its p->state before coming back.
+        c->proc = 0;
+    
+      }
+      // Switch to chosen process.  It is the process's job
+      // to release ptable.lock and then reacquire it
+      // before jumping back to us.
+      // c->proc = p;
+      // switchuvm(p);
+      // p->state = RUNNING;
+
+      // swtch(&(c->scheduler), p->context);
+      // switchkvm();
+
+      // // Process is done running for now.
+      // // It should have changed its p->state before coming back.
+      // c->proc = 0;
     }
     release(&ptable.lock);
     // cprintf("########scheduler4########\n");
