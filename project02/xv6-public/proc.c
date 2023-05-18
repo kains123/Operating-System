195286439258425fbb1e4bed37c9711c7a3401e2
kains123 <kains0417@naver.com>
2023-05-18 14:08:28 +0900

    Finish

--- project02/xv6-public/proc.c
+++ project02/xv6-public/proc.c
@@ -498,17 +498,18 @@ sched(void)
 {
   int intena;
   struct proc *p = myproc();
+  struct thread *t = &CURTHREAD(p);
 
   if(!holding(&ptable.lock))
     panic("sched ptable.lock");
   if(mycpu()->ncli != 1)
     panic("sched locks");
-  if(p->state == RUNNING)
+  if(t->state == RUNNING)
     panic("sched running");
   if(readeflags()&FL_IF)
     panic("sched interruptible");
   intena = mycpu()->intena;
-  swtch(&p->context, mycpu()->scheduler);
+  swtch(&t->context, mycpu()->scheduler);
   mycpu()->intena = intena;
 }
 
