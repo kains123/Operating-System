6aaf65d27b8473389ce4cd651123a618020caa53
kains123 <kains0417@naver.com>
2023-05-18 16:01:56 +0900

    Lack...

--- project02/xv6-public/init.c
+++ project02/xv6-public/init.c
@@ -29,7 +29,6 @@ main(void)
     if(pid == 0){
       // exec("sh", argv);
       //TODO
-      printf(1, "&&&HERE&&&\n");
       exec2("sh", argv, 100);
       printf(1, "init: exec sh failed\n");
       exit();
