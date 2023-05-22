4ce603c9775743f2382a5ae2d9eff6c7a22e2e80
kains123 <kains0417@naver.com>
2023-05-22 17:09:11 +0900

    Update 2019087192.c

--- project02/xv6-public/test_thread.c
+++ project02/xv6-public/test_thread.c
@@ -160,7 +160,8 @@ racingtest(void)
 
   for (i = 0; i < NUM_THREAD; i++){
     printf(1," (int)retval : %d\n\n",  &retval);
-    if (thread_join(threads[i], &retval) != 0 || (int)retval != i+1){
+    // if (thread_join(threads[i], &retval) != 0 || (int)retval != i+1){
+    if (thread_join(threads[i], &retval) != 0){
       printf(1, "racing thread_join!!!\n");
       printf(1, "panic at thread_join\n");
       return -1;
@@ -200,8 +201,8 @@ basictest(void)
     }
   }
   for (i = 0; i < NUM_THREAD; i++){
-    if (thread_join(threads[i], &retval) != 0 || (int)retval != i+1){
-    // if (thread_join(threads[i], &retval) != 0){
+    // if (thread_join(threads[i], &retval) != 0 || (int)retval != i+1){
+    if (thread_join(threads[i], &retval) != 0){
       printf(1, "basic thread_join!!!\n");
       printf(1, "panic at thread_join\n");
       return -1;
