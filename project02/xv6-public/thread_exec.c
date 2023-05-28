a7cf12dc1825f0ed26d83207bd009290d6e39a49
kains123 <kains0417@naver.com>
2023-05-28 13:10:44 +0900

    Update 2019087192.c

--- project02/xv6-public/thread_exec.c
+++ project02/xv6-public/thread_exec.c
@@ -13,7 +13,7 @@ void *thread_main(void *arg)
     char *pname = "/hello_thread";
     char *args[2] = {pname, 0};
     printf(1, "Executing...\n");
-    exec2(pname, args, 50);
+    exec(pname, args);
   }
   else {
     sleep(200);
