c3c78f2c8bd53d3e0d7a5ccf019897f54ceca468
kains123 <kains0417@naver.com>
2023-05-30 17:22:24 +0900

    Update 2019087192.c

--- project03/xv6-public/ln.c
+++ project03/xv6-public/ln.c
@@ -10,12 +10,12 @@ main(int argc, char *argv[])
     exit();
   }
   printf(2, "%s********\n",argv[1]);
-  if(!strcmp(argv[1], "-s")) {
+  if(strcmp(argv[1], "-h") == 0) {
     if(link(argv[2], argv[3]) < 0) {
       printf(2, "link %s %s: failed\n", argv[1], argv[2]);
     }
   }
-  if(!strcmp(argv[1], "-h")) {
+  if(strcmp(argv[1], "-s") == 0) {
     if (symlink(argv[2], argv[3]) < 0) {
       printf(2, "symlink %s %s: failed\n", argv[2], argv[3]);
     }
