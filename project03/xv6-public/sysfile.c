316d3005b01bddbe43798195b5e08be66eb460f0
kains123 <kains0417@naver.com>
2023-06-08 18:30:02 +0900

    Update 2019087192.c

--- project03/xv6-public/sysfile.c
+++ project03/xv6-public/sysfile.c
@@ -343,23 +343,27 @@ sys_open(void)
       return -1;
     }
     if(ip->type == T_SYMLINK && (omode != O_NOFOLLOW)) { 
+      int count = 0;
       do {
         cprintf("HERE!.\n");
         readi(ip, (char*)&length, 0, sizeof(int));
         readi(ip, path, sizeof(int), length + 1);
         iunlockput(ip);
-
-        
         if((ip = namei(path, 64)) == 0){
-          
-          
           cprintf("Error: Inode cannot found. Original file could be deleted or possible inode corruption occured.\n");
           end_op();
           return -1;
         }
         ilock(ip); 
-      
-      } while(ip->type == T_SYMLINK);
+        count++;
+
+        if(count >= 10){
+            cprintf("Cycle!\n");
+            iunlockput(ip);
+            end_op(ROOTDEV);
+            return -1;
+        }
+      } while(ip->type == T_SYMLINK && count < 10);
     }
   }
 
